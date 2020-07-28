9eb159750e04a48f7b0395158697501ff6435adf
fnaji <faris.naji@tercen.com>
2020-07-28 10:17:05 +0000

    added renv

--- main.R
+++ main.R
@@ -9,7 +9,7 @@ if(inherits(try(ctx$select(".x")), 'try-error')) stop("x axis is missing.")
 ctx %>%
   select(.x, .y, .ri, .ci) %>% 
   group_by(.ri, .ci) %>%
-  mutate(seq = as.numeric(1:length(.x)), x_axis = .x) %>%
+  mutate(seq = as.numeric(1:length(.x))) %>%
   ungroup()%>%
   select(seq) %>%
   ctx$addNamespace() %>%
